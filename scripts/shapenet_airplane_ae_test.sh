c08274b1dfdd30f09c104251df8e70476ed64701
Linhao Wang <150442599+wanglh300@users.noreply.github.com>
2025-02-28 15:47:25 +0800

    Delete scripts/shapenet_airplane_ae_test.sh

--- scripts/shapenet_airplane_ae_test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-python test.py \
-    --cates airplane \
-    --resume_checkpoint  checkpoints/ae/shapenet15k-cateairplane/checkpoint-899.pt \
-    --dims 512-512-512 \
-    --use_deterministic_encoder \
-    --evaluate_recon
